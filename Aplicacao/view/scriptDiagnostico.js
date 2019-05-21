9484a9c2cff2b63d0f4529b76989076e83b7da11
brunodias3 <42379869+brunodias3@users.noreply.github.com>
2019-05-21 20:31:55 -0300

    front criacao do paciente

--- Aplicacao/view/scriptDiagnostico.js
+++ Aplicacao/view/scriptDiagnostico.js
@@ -4,6 +4,11 @@ function editavel(){
 	for(i=0; i<forms.length; i++){
 		forms[i].disabled = false;
 		forms[i].classList.remove("form-control-plaintext");
+		if(forms[i].id != "dataConsulta"){		
+			forms[i].type = "number";
+			forms[i].min = "0";
+			forms[i].step = "0.01";
+		}
 	}
 	document.getElementById("botaoAlterar").style.display = "none";
 	document.getElementById("botaoSalvar").style.display = "inline";
