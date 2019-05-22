e8fbd6909f39f0251e1d8a49297bdeeeeb991cfb
brunodias3 <42379869+brunodias3@users.noreply.github.com>
2019-05-22 18:08:35 -0300

    novo nutricionista

--- Aplicacao/view/scriptNutri.js
+++ Aplicacao/view/scriptNutri.js
@@ -2,3 +2,8 @@ function selecionaPaciente(id){
 	document.getElementById("paciente").value = id;
 	document.getElementById("pacienteEscolhido").submit();
 }
+
+function removePaciente(id){
+	document.getElementById("paciente").value = id;
+	document.getElementById("pacienteEscolhido").submit();
+}
